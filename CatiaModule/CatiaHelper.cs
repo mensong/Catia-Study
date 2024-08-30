9cb0fc5fdac0c427cede58c46f1cd5d903709e54
lims62 <lims62@meicloud.com>
2024-08-30 16:08:25 +0800

    initial

---
+++ CatiaModule/CatiaHelper.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatiaModule
+{
+    public class CatiaHelper
+    {
+        private static INFITF.Application _catiaApp = null;
+        public static INFITF.Application App 
+        { 
+            get 
+            {
+                if (_catiaApp != null)
+                    return _catiaApp;
+                _catiaApp = (INFITF.Application)Marshal.GetActiveObject("Catia.Application");
+                return _catiaApp;
+            }
+        }
+
+        /// <summary>
+        /// ProductStructureTypeLib.ProductDocument
+        /// DRAFTINGITF.DrawingDocument
+        /// PPR.PPRDocument
+        /// CATFunctSystem.FunctionalDocument
+        /// CATMat.MaterialDocument
+        /// ComponentsCatalogsTypeLib.CatalogDocument
+        /// MECMOD.PartDocument
+        /// PROCESSITF.ProcessDocument
+        /// SAMITF.AnalysisDocument
+        /// 这些可以在对象浏览器里搜索Document查得到
+        /// </summary>
+        public static INFITF.Document ActiveDoc
+        {
+            get 
+            {
+                return App.ActiveDocument;
+            }
+            set
+            {
+                value.Activate();
+            }
+        }
+
+        public static dynamic ExecuteVBA(
+            string iLibraryFilePath, 
+            string iProgramName, 
+            string iFunctionName, 
+            IEnumerable<object> iParameters = null)
+        {
+            Array parameters = null;
+            if (iParameters != null)
+                parameters = iParameters.ToArray();
+            var res = App.SystemService.ExecuteScript(
+                ref iLibraryFilePath,
+                INFITF.CatScriptLibraryType.catScriptLibraryTypeVBAProject,
+                ref iProgramName,
+                ref iFunctionName,
+                parameters);
+            return res;
+        }
+
+        public static dynamic ExecuteVBAScript(string script,
+            string functionName = null, IEnumerable<object> iParameters = null)
+        {
+            if (string.IsNullOrEmpty(functionName))
+            {//如果没有传入functionName，则构造一个以消除执行错误
+                functionName = "ExecuteVBAScript_helper";
+                script += "\r\nFunction ExecuteVBAScript_helper()\r\nEnd Function";
+            }
+            var res = App.SystemService.Evaluate(script,
+                INFITF.CATScriptLanguage.CATVBScriptLanguage, functionName, null);
+            return res;
+        }
+
+    }
+}
---
+++ CatiaModule/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatiaModule
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 获取当前活动ProductDocument
+            ProductStructureTypeLib.ProductDocument prdDoc =
+                (ProductStructureTypeLib.ProductDocument)CatiaHelper.ActiveDoc;
+            // 创建一个ID为newProduct的Product
+            var p = prdDoc.Product.Products.AddNewProduct("newProduct");
+
+            //Catia.StartCommand("LoginCmd");
+
+            //string iLibraryName = @"G:\CatiaMacro\VBA 项目2.catvba";
+            //string iProgramName = @"模块1";
+            //string iFunctionName = @"myfoo";
+            //CatiaHelper.ExecuteVBA(iLibraryName, iProgramName, iFunctionName);
+
+            //CatiaHelper.App.SystemService.Evaluate(
+            //    "Function myfoo()\r\nMsgBox \"ok\"\r\nmyfoo = \"我是返回值\"\r\nEnd Function",
+            //    INFITF.CATScriptLanguage.CATVBScriptLanguage, "myfoo", null);
+
+            //CatiaHelper.ExecuteVBAScript("MsgBox \"ok\"");
+            //CatiaHelper.ExecuteVBAScript("Function myfoo()\r\nMsgBox \"ok\"\r\nmyfoo = \"我是返回值\"\r\nEnd Function", "myfoo");
+
+
+        }
+    }
+}
